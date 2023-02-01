47238d6ac66ea4baf4b402fd9d84f414cd777c1f
Neeraj Gupta <neerajgupta.mbox@gmail.com>
2023-02-01 23:05:14 +0530

    Deleted the table

--- packages/components/table/lib/code/TableDataContext.js
+++
@@ -1,5 +0,0 @@
-import {createContext} from "react";
-
-const TableDataContext = createContext({});
-
-export default TableDataContext;
--- packages/components/table/lib/code/common/operationsTypes.js
+++
@@ -1,3 +0,0 @@
-export const ADD = 'ADD';
-export const PATCH = 'PATCH';
-export const DELETE = 'DELETE';
